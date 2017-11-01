65d4b5ae371997109bab062f84e35e773fa1e87c
Marc Oliva <ing.oliva@hotmail.com>
2017-11-01 02:17:39 -0500

    Implementation ProductContract class and ProductDbHelper class

---
+++ app/src/main/java/com/example/marcoliva/inventoryapp/data/ProductContract.java
@@ -0,0 +1,43 @@
+package com.example.marcoliva.inventoryapp.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by ThinkSoft on 1/11/2017.
+ */
+
+public class ProductContract {
+    //Create Uri
+    public static final String CONTENT_AUTHORITY ="com.example.marcoliva.inventoryapp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+    public static final String PATH_PRODUCTS = "products";
+
+    public static  class ProductEntry implements BaseColumns{
+        public static  final String TABLE_NAME ="products";
+        public static  final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_PRODUCTS);
+
+        public static  final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PRODUCT_NAME = "name";
+        public static final String COLUMN_PRODUCT_STOCK ="stock";
+        public static final String COLUMN_PRODUCT_PRICE="price";
+        public static final String COLUMN_PRODUCT_IMAGE= "image";
+
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of products.
+         */
+        public static  final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE+"/"+CONTENT_AUTHORITY+"/"+PATH_PRODUCTS;
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single product.
+         */
+        public static  final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE+"/"+CONTENT_AUTHORITY+"/"+PATH_PRODUCTS;
+
+
+
+    }
+
+}
