f1c674b2fad57fd6bbfba1e97114efcc87b784f4
Marc Oliva <ing.oliva@hotmail.com>
2017-11-02 20:03:03 -0500

    Implementation of the UI

--- app/src/main/java/com/example/marcoliva/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/marcoliva/inventoryapp/EditorActivity.java
@@ -5,7 +5,10 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by ThinkSoft on 1/11/2017.
@@ -18,6 +21,27 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
         setTitle("Add a Product");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_save:
+                return true;
+            case R.id.action_delete:
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(EditorActivity.this);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
